cc22fddebe8ff4a6efaaf08dcdbbf9a821cb5e01
deatiger <deatiger1492@gmail.com>
2021-07-22 12:04:44 +0900

    feat: Add a feature to fetch user data from GitHub API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import {BadCounter, Counter, TextInput, ToggleButton} from "./components";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [name, setName] = useState('')
+  const [id, setId] = useState('deatiger')
+  const ids = ['deatiger', 'gaearon', 'aws', 'google', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setName(data.name)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  },[id])
+
   return (
     <div>
-      {/*<TextInput />*/}
-      {/*<BadCounter />*/}
-      {/*<Counter />*/}
-      <ToggleButton />
+      <p>{id}の、GitHub上の名前は{name}です。</p>
+      <button onClick={getRandomId}>IDを変更</button>
     </div>
   );
 }
